381dda1891cb6ef7e7c024e4b5edcbdc7752d4ff
Aleem0001 <aleemhassan101@gmail.com>
2022-09-10 07:30:41 -0700

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: ALways return 0
+ * Return: ALway returns 0
  */
 {
 	char a;
 	for (a = 'a'; a <= 'z' ; a++)
 		putchar(a);
 	putchar('\n');
-	return (0)
+	return (0);
 }
