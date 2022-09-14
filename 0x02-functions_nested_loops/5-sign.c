1ada430b9c42a21a6db28343335bbc5b342dc592
Aleem0001 <aleemhassan101@gmail.com>
2022-09-13 20:02:54 -0700

    ps

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * print_sign - prints the sign of a number
+ * print_sign - function to check for a sign of a number
  *@n: is the int that will use for the argument of the function
  * Return: 0
  */
@@ -12,16 +12,14 @@ int print_sign(int n)
 	return (1);
 }
 
-	else if (n == 0)
-	{
-	_putchar('0');
-	return (0);
-	}
 	else if (n < 0)
 	{
 	_putchar('-');
 	return (-1);
 	}
+	else
+	{
+	_putchar('0');
 	return (0);
-
+	}
 }
