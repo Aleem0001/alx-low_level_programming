a7ff9ae12adead9c9fad588f052b000497b010a8
Aleem0001 <aleemhassan101@gmail.com>
2022-09-13 21:16:30 -0700

    hr

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,15 +12,15 @@ void jack_bauer(void)
 	int hours_remainder;
 	int mins_remainder;
 
-	while (hours <= 24)
+	while (hours <= 23)
 	{
-	while (minutes <= 60)
+	while (minutes <= 59)
 	{
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
-	_putchar('1');
+	_putchar(':');
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');
 	minutes++;
