682120dd4350a69ddc5e8749548d18181275ea23
Aleem0001 <aleemhassan101@gmail.com>
2022-09-13 19:16:13 -0700

    slow

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _inslower - function to check for lowercase character
+ * _islower - function to check for lowercase character
  * @c:	is the int that will use for argument of the function
  * Return: 0
  */
