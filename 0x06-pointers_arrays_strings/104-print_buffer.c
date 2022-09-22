52d254d1155999f736152ddd121d2917fc09d682
Aleem0001 <110991955+Aleem0001@users.noreply.github.com>
2022-09-23 00:40:15 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
  * print_buffer - print a buffer 10 bytes at a time, displaying the line
  * number in hex and byte pairs in hex
+ *
  * @b: string to print from
  * @size: size of buffer
+ *
  * Return: void
  */
 void print_buffer(char *b, int size)
