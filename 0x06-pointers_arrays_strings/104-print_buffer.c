e9dff4bebc2fc4138be02bd6b09eef03b2f14b50
Aleem0001 <110991955+Aleem0001@users.noreply.github.com>
2022-09-23 04:56:26 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,53 +1,61 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * print_buffer - print a buffer 10 bytes at a time, displaying the line
- * number in hex and byte pairs in hex
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
  *
- * @b: string to print from
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+	88	else
+			putchar('.');
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
  * @size: size of buffer
  *
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int i, counter = 0, size2 = size;
-	char *ptr = b;
+	int i;
 
-	if (size == 0)
-		printf("\n");
-	while (size > 0)
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf("%08x:", counter);
-		i = 0;
-		while (size > 0 && i < 10)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
-			if (!(i % 2))
-				printf(" ");
-			printf("%02x", *ptr++);
-			i++;
-			size--;
-			counter++;
+			print_line(b, 9, i);
 		}
-		while (counter % 10 != 0)
+		else
 		{
-			if (!(counter % 2))
-				printf(" ");
-			printf("  ");
-			counter++;
+			print_line(b, size % 10 - 1, i);
 		}
-		printf(" ");
-		i = 0;
-		while (size2 > 0 && i < 10)
-		{
-			if (*b > 31 && *b != 127)
-				printf("%c", *b);
-			else
-				printf(".");
-			size2--;
-			i++;
-			b++;
-		}
-		printf("\n");
+		putchar('\n');
 	}
+	if (size == 0)
+		putchar('\n');
 }
